394919059a4008fe6e4e75ee7991c114e9e3ed5f
csae8092 <peter.andorfer@oeaw.ac.at>
2021-06-01 13:15:25 +0200

    removed templateview

--- netvis/urls.py
+++ netvis/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('graph/<app_name>/<model_name>', views.qs_graph_data, name='qs_as_graph'),
     path('cached/<app_name>/<model_name>', views.cashed_graph_data, name='cached_graph'),
     path('preview/<app_name>/<model_name>', views.preview_graph_data, name='preview_graph'),
-    path('<app_name>/<model_name>', views.CachednetvisView.as_view(), name='cached_graph_html'),
 ]
--- netvis/views.py
+++ netvis/views.py
@@ -4,7 +4,6 @@ from django.http import JsonResponse
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.contenttypes.models import ContentType
-from django.views.generic import TemplateView
 
 from . models import NetVisCache
 from . utils import as_graph, qs_as_graph
@@ -54,17 +53,3 @@ def preview_graph_data(request, app_name, model_name):
         return JsonResponse({})
     graph = json.loads(item.graph_data_preview)
     return JsonResponse(graph)
-
-
-class CachednetvisView(TemplateView):
-    template_name = 'netvis/gen_netvis.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(CachednetvisView, self).get_context_data(**kwargs)
-        context["app_name"] = kwargs['app_name']
-        context["model_name"] = kwargs['model_name']
-        context["graph_url"] = reverse('netvis:cached_graph', kwargs={
-            'app_name': kwargs['app_name'],
-            'model_name': kwargs['model_name']
-        })
-        return context
