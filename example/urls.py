5090b6b761f065eb2b415c8ca19f4fcab0a81447
csae8092 <peter.andorfer@oeaw.ac.at>
2021-06-07 12:20:14 +0200

    more examples

--- example/urls.py
+++ example/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('my-text/<int:pk>/', views.MyTextDetailView.as_view(), name='mytext_detail'),
+    path('my-text/<int:pk>/', views.MyTextDetailViewSimple.as_view(), name='mytext_simple'),
+    path('m/<int:pk>/', views.MyTextDetailView.as_view(), name='mytext'),
+    path('', views.IndexView.as_view(), name="index")
 ]
--- example/views.py
+++ example/views.py
@@ -1,8 +1,20 @@
 from django.views.generic.detail import DetailView
+from django.views.generic.base import TemplateView
+
 from example.models import MyText
+
+class IndexView(TemplateView):
+
+    template_name = 'example/index.html'
+
+
+class MyTextDetailViewSimple(DetailView):
+
+    model = MyText
+    template_name = 'example/mytext_simple.html'
 
 
 class MyTextDetailView(DetailView):
 
     model = MyText
-    template_name = 'example/mytext_detail.html'
+    template_name = 'example/mytext.html'
